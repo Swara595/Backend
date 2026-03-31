94f7d564dfd7b6ebbdda46d9a12bb0d410b39d8f
Swaralee <swaraleesadul17650@gmail.com>
2026-03-31 22:23:19 +0530

    first commit

---
+++ day2-programmingServer/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+// server create ho chuka hai
+
+app.get('/', (req, res) => {
+    res.send("Hello world!! Server is now working")
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is the about page")
+})
+
+app.get('/contact', (req, res) => {
+    res.send("This is the contact page")
+})
+
+app.listen(3000)
+// server start ho chuka hai
